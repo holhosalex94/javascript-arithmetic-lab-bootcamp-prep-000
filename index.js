11ee704e75dd8936b95568739997e192021d9ea0
Alex Holhos <alex.holhos@emanuel.ro>
2018-02-12 18:49:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function add(a, b){
  return a+b;
 }
 function subtract(a, b){
- return b-a;
+ return a-b;
 }
 function multiply(a, b){
  return a*b;
@@ -17,5 +17,5 @@ function dec(n){
  return n-1;
 }
 function makeInt(n){
- return n.parseInt();
+ return parseInt(n);
 }
